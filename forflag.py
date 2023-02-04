a8cbfedca40be2d143600ead671d07e4a936f5e0
mazhongchao <mazhongchao@gmail.com>
2023-02-04 17:12:06 +0800

    init commit

---
+++ forflag.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import time
+import subprocess
+
+
+def run():
+
+    with open('/Users/mach/projects/last-commit.txt', 'w+') as f:
+        text = f.read()
+        text_list = text.split('|')
+        date = text_list[0]
+        stat = text_list[1]
+
+        if date == today and stat == '1':
+            return
+
+        cmd_list = ["git add .", "git commit -m 'update'", 'git push origin main']
+
+        # ret是类subprocess.CompletedProcess对象(args, returncode(0=成功), stdout子进程输出, stderr,check_returncode()
+        ret = subprocess.run(cmd_list,
+          shell = True,
+          timeout = 10,
+          stdout = subprocess.PIPE,
+          stdin = subprocess.PIPE)
+
+        today = time.strftime("%Y-%m-%d", time.localtime())
+        if ret.returncode == 0:
+            f.write(f"{today}|1")
+        else:
+            f.write(f"{today}|0")
+
+
+if __name__ == "__main__":
+
+    run()
+
